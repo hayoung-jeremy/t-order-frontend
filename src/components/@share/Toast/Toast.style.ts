6aff32cee8a84629f84354c692aa28640816a38f
Ha young, Kim <jeremiah91@naver.com>
2021-09-08 00:47:51 +0900

    feat : 주문하기
     - [주문이 완료되었습니다] 메시지 박스가 나타나며, 3초 후 사라진다.

---
+++ src/components/@share/Toast/Toast.style.ts
@@ -0,0 +1,28 @@
+import PALETTE from "constants/palette";
+import styled from "styled-components";
+
+const StyledToast = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  z-index: 3;
+  transform: translate(-50%, -50%);
+  width: 62.5vw;
+  height: 15.625vw;
+  border-radius: 10px;
+  background-color: ${PALETTE.MAIN};
+  color: ${PALETTE.WHITE};
+  font-size: 5.625vw;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: 0.3s;
+
+  &.hide {
+    opacity: 0;
+    top: 100%;
+  }
+`;
+
+export default StyledToast;
--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import StyledCart from "./Cart.styles";
 import CartListItem from "./CartListItem/CartListItem";
 import Button from "components/@share/Button/Button";
 import { useAppDispatch, useAppSelector } from "features/store/rootReducer";
 import { clearCart, getTotal, toggleCartOpen } from "features/cart/cartReducer";
+import Toast from "components/@share/Toast/Toast";
 
 const Cart = () => {
   const cart = useAppSelector((state) => state.cart);
@@ -12,6 +13,8 @@ const Cart = () => {
   useEffect(() => {
     dispatch(getTotal());
   }, [cart, dispatch]);
+
+  const [isActive, setIsActive] = useState(false);
   const handleClearCart = () => {
     dispatch(clearCart());
   };
@@ -19,49 +22,57 @@ const Cart = () => {
     dispatch(toggleCartOpen());
   };
   return (
-    <StyledCart className={cart.isCartOpen ? "" : "hide"}>
-      <div className="cart-header">
-        <h3 className="cart-title">장바구니</h3>
-      </div>
-      <div className="cart-body">
-        {cart.cartItems.length === 0 ? (
-          <p className="empty-sign">장바구니가 비어있습니다.</p>
-        ) : (
-          cart.cartItems.map((cartItem) => (
-            <CartListItem key={cartItem.itemId} cartItem={cartItem} />
-          ))
-        )}
-      </div>
-      <div className="cart-footer">
-        <div className="cart-item-info">
-          <span>총 {cart.cartItems.length}가지</span>
-          <span className="cart-item-total-price">
-            합계 <span>{cart.cartTotalAmount?.toLocaleString()}원</span>
-          </span>
+    <>
+      <Toast
+        message="주문이 완료되었습니다."
+        isActive={isActive}
+        setIsActive={setIsActive}
+      />
+      <StyledCart className={cart.isCartOpen ? "" : "hide"}>
+        <div className="cart-header">
+          <h3 className="cart-title">장바구니</h3>
         </div>
-        <div className="cart-controller">
-          <Button
-            color="WHITE"
-            bgColor="GREY600"
-            fontWeight="bold"
-            onClick={() => handleCartOpen()}
-          >
-            닫기
-          </Button>
-          <Button
-            color="WHITE"
-            bgColor="MAIN"
-            fontWeight="bold"
-            onClick={() => {
-              handleClearCart();
-              handleCartOpen();
-            }}
-          >
-            주문하기
-          </Button>
+        <div className="cart-body">
+          {cart.cartItems.length === 0 ? (
+            <p className="empty-sign">장바구니가 비어있습니다.</p>
+          ) : (
+            cart.cartItems.map((cartItem) => (
+              <CartListItem key={cartItem.itemId} cartItem={cartItem} />
+            ))
+          )}
         </div>
-      </div>
-    </StyledCart>
+        <div className="cart-footer">
+          <div className="cart-item-info">
+            <span>총 {cart.cartItems.length}가지</span>
+            <span className="cart-item-total-price">
+              합계 <span>{cart.cartTotalAmount?.toLocaleString()}원</span>
+            </span>
+          </div>
+          <div className="cart-controller">
+            <Button
+              color="WHITE"
+              bgColor="GREY600"
+              fontWeight="bold"
+              onClick={() => handleCartOpen()}
+            >
+              닫기
+            </Button>
+            <Button
+              color="WHITE"
+              bgColor="MAIN"
+              fontWeight="bold"
+              onClick={() => {
+                handleClearCart();
+                handleCartOpen();
+                setIsActive(true);
+              }}
+            >
+              주문하기
+            </Button>
+          </div>
+        </div>
+      </StyledCart>
+    </>
   );
 };
 
